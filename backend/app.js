94b028e322c94333a8410b8b41a10cbf74508b0c
boranorben <zhotkp@gmail.com>
2020-05-26 02:06:36 +0700

    add dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,8 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = 4000
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'database-2.cjttdajuhgpx.us-east-1.rds.amazonaws.com',
@@ -5,12 +10,20 @@ const connection = mysql.createConnection({
   password: 'cloudpj.',
   database: 'myDatabase'
 });
+
+app.use(cors())
+app.use(bodyParser.json())
+
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected!');
-//   sql="insert into brownies_tb (brownies_id, brownies_name, brownies_price) values('5', 'bb', '50')";
-//   connection.query(sql, function(err, result){
-//     if(err) throw err;
-//     console.log("Insert complete");
-//   });
+  console.log('MySQL connected!');
+  //   sql="insert into brownies_tb (brownies_id, brownies_name, brownies_price) values('5', 'bb', '50')";
+  //   connection.query(sql, function(err, result){
+  //     if(err) throw err;
+  //     console.log("Insert complete");
+  //   });
 });
+
+app.listen(PORT, () => {
+  console.log('Server is running on Port: ' + PORT)
+})
